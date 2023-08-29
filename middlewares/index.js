73d3bad4a1f0955a89b5465a010ce6f836dce129
Utilizator <Stanislavbox24@gmail.com>
2023-08-29 16:03:28 +0300

    Added user avatar loading via Multer

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,6 +3,7 @@ const isValidId = require("./isValidId");
 const validateBodyFavorite = require("./validateBodyFavorite");
 const authenticate = require("./authenticate");
 const validateSubscription = require("./validSubscriptions");
+const upload = require("./upload");
 
 module.exports = {
   validateBody,
@@ -10,4 +11,5 @@ module.exports = {
   validateBodyFavorite,
   authenticate,
   validateSubscription,
+  upload,
 };
