c57ea67412c778fb15f0cfb8f6a0cbab83de2356
Utilizator <Stanislavbox24@gmail.com>
2023-09-07 20:45:34 +0300

    connected Elastic Email

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
+const ElasticEmail = require("@elasticemail/elasticemail-client");
+
 require("dotenv").config();
+
+const { ELASTIC_API_KEY, EMAIL_FROM } = process.env;
+
+const defaultClient = ElasticEmail.ApiClient.instance;
+
+const { apikey } = defaultClient.authentications;
+apikey.apiKey = ELASTIC_API_KEY;
+
+const api = new ElasticEmail.EmailsApi();
+
+const email = ElasticEmail.EmailMessageData.constructFromObject({
+  Recipients: [new ElasticEmail.EmailRecipient("wipexi8323@nickolis.com")],
+  Content: {
+    Body: [
+      ElasticEmail.BodyPart.constructFromObject({
+        ContentType: "HTML",
+        Content: "<strong>Test Email!</strong>",
+      }),
+    ],
+    Subject: "Test Email",
+    From: EMAIL_FROM,
+  },
+});
+
+const callback = function (error, data, response) {
+  if (error) {
+    console.error(error.message);
+  } else {
+    console.log("API called successfully.");
+  }
+};
+
+api.emailsPost(email, callback);
+
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
@@ -12,7 +48,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 
 app.use(express.json());
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.use("/users", authRouter); //
 app.use("/api/contacts", contactsRouter);
